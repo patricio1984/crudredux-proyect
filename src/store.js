2058c3ff00a0390739a279d6a295536a70e9e2b4
patricio1984 <patricio1984@gmail.com>
2021-04-09 21:07:22 -0300

    Corrección en store.js

--- src/store.js
+++ src/store.js
@@ -4,10 +4,12 @@ import reducer from "./reducers";
 
 const store = createStore(
     reducer,
-    compose(applyMiddleware(thunk),
-    typeof window === "object" &&
-        typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" ?
-        window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    compose(applyMiddleware(thunk)
+    
+    // ,
+    // typeof window === "object" &&
+    //     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" ?
+    //     window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 );
 
